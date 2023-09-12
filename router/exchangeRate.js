d67e26c2d617ae65f69f81a3950f8bb4e193b465
Yuan Huan <yuanhuan@lxrental.com>
2023-09-12 15:53:05 +0800

    *

--- router/exchangeRate.js
+++ router/exchangeRate.js
@@ -5,10 +5,33 @@ const exchangeRouter = new Router({
   prefix: '/api/exchangeRate'
 })
 
-// 获取汇率信息
+// 获取汇率列表
+exchangeRouter.get('/getquotation/list', async(ctx, next) => {
+  let params = {
+    market: 'foreign',
+    finClientType: 'pc',
+  };
+
+  let data = await request.get('https://finance.pae.baidu.com/api/getbanner', {params})
+
+  console.log(data)
+  ctx.response.body = data.Result;
+
+})
+
+// 根据code获取汇率信息
 exchangeRouter.get("/getquotation", async (ctx, next) => {
 
-  let data = await request.get('https://finance.pae.baidu.com/vapi/v1/getquotation?group=huilv_minute&need_reverse_real=1&code=USDCNH&finClientType=pc')
+  let query = ctx.request.query;
+
+  let params = {
+    group: 'huilv_minute',
+    need_reverse_real: 1,
+    finClientType: 'pc',
+    ...query
+  }
+
+  let data = await request.get('https://finance.pae.baidu.com/vapi/v1/getquotation', {params})
 
   ctx.response.body = data.Result;
 
